ad8aaac96f4e9e7970d987f9dbfb1dc7ac7b3384
Mutave Chara <mutave49@gmail.com>
2022-11-16 21:05:17 -1000

    Show user correct usage of the tool

--- recon.sh
+++ recon.sh
@@ -4,7 +4,7 @@ host $1 | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-
 #first we read the domain as a command line argument and pass it to the host tool
 #we the find all the IP addresses (with regex) in the "host" result and output only them to the console so that we can save the IPs
 
-if [-z "$1"]; then
+if [ -z "$1" ]; then
 	echo "no domain has been provided"
 	echo "usage : ./recon.sh domain.com"
 	exit
