533053ff845b17d0190a1c65de58ab0bb020faee
Mutave Chara <mutave49@gmail.com>
2022-11-16 22:25:32 -1000

    Use ping command to check if the domain provided responds to ping

--- recon.sh
+++ recon.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-host $1 | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-#first we read the domain as a command line argument and pass it to the host tool
-#we the find all the IP addresses (with regex) in the "host" result and output only them to the console so that we can save the IPs
+
 
 HELP()
 {
+	#Display Help
+
 	echo "Automated recon script for Bug Bounty by MutaveChara"
+	echo
 	echo "Syntax: recon.sh [-h|v|V] <targetdomain.com>"
 	echo "options:"
-	echo "h  Print this Help."
-	echo "v  Verbose mode."
-	echo "V  Print software version and exit."
+	echo "h	  Print this Help."
+	echo "v	  Verbose mode."
+	echo "V	  Print software version and exit."
+	echo
 }
 
 if [ -z "$1" ]; then
 	HELP
 	exit
-fi
 #added a HELP() function in case the user does not put in the right input, we show them how to use the tool correctly 
+
+elif ping -c 1 $1 &> /dev/null; then
+	echo "Host reachable"
+	host $1 | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+
+#first we read the domain as a command line argument and pass it to the host tool
+#we the find all the IP addresses (with regex) in the "host" result and output only them to the console so that we can save the IPs
+#use the ping command to check if the domain provided by the user does respond to ping
+
+else
+	echo "Host Unavailable!"
+	exit
+fi
